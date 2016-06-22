8651e2f6aacc75d8e4e878c57139efba820e65ac
John <jmangel@tesla.com>
2016-06-22 11:51:39 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function(value, options, key, attributes) {
+  console.log(value);
+  console.log(options);
+  console.log(key);
+  console.log(attributes);
+  return "is totally wrong";
+};
